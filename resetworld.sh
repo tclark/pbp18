9e63e4957e6a61c4993fdef88afd83e9f449695a
Tom Clark <tom@runawayplay.com>
2018-04-16 13:00:52 +1200

    fixed broken path in resetworld.sh

--- resetworld.sh
+++ resetworld.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo 'Resetting Young Coders Minecraft world..'
-cp -r ~/pbp/mcworlds/world ~/.minecraft/games/com.mojang/minecraftWorlds/
+cp -r ~/pbp18/mcworlds/world ~/.minecraft/games/com.mojang/minecraftWorlds/
 echo 'Done.'
